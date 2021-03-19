340cab4aa255eff6e55bfc0767ed2d0c45508bbb
Kazuki Kaneoka <kzkaneoka@gmail.com>
2021-03-19 15:56:42 +0900

    init: started bbs app for spring boot

---
+++ src/test/java/com/github/kzkaneoka/bbs/BbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kzkaneoka.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
