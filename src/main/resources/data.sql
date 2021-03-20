fae17dc8257edfafc0af0e9e9debb7ec09c6c6f1
Kazuki Kaneoka <kzkaneoka@gmail.com>
2021-03-20 10:10:47 +0900

    feat: created data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (id, username, email, password, role, status) VALUES ('e34fcf62d5474b36a36ffdb324251101', 'user1', 'user1@gmail.com', '123456', 'NORMAL', 'ACTIVE');
+INSERT INTO users (id, username, email, password, role, status) VALUES ('e34fcf62d5474b36a36ffdb324251102', 'user2', 'user2@gmail.com', '123456', 'NORMAL', 'ACTIVE');
+INSERT INTO users (id, username, email, password, role, status) VALUES ('e34fcf62d5474b36a36ffdb324251103', 'user3', 'user3@gmail.com', '123456', 'NORMAL', 'ACTIVE');
+INSERT INTO users (id, username, email, password, role, status) VALUES ('e34fcf62d5474b36a36ffdb324251104', 'user4', 'user4@gmail.com', '123456', 'NORMAL', 'ACTIVE');
+
+INSERT INTO users (id, username, email, password, role, status) VALUES ('f8ab326b6a394c1c95afa22a837f2251', 'admin1', 'admin1@gmail.com', '123456', 'ADMIN', 'ACTIVE');
+INSERT INTO users (id, username, email, password, role, status) VALUES ('f8ab326b6a394c1c95afa22a837f2252', 'admin2', 'admin2@gmail.com', '123456', 'ADMIN', 'ACTIVE');
+
+INSERT INTO users (id, username, email, password, role, status) VALUES ('350221f0c7fb45389fb7d31395e744f1', 'moderator1', 'moderator1@gmail.com', '123456', 'MODERATOR', 'ACTIVE');
+INSERT INTO users (id, username, email, password, role, status) VALUES ('350221f0c7fb45389fb7d31395e744f2', 'moderator2', 'moderator2@gmail.com', '123456', 'MODERATOR', 'ACTIVE');
+
+--INSERT INTO forms (id, title, description, status, user_id) VALUES ('4c3dda31ab354565af11c3c2395651c1', 'form1', 'this is form1', 'OPEN', 'e34fcf62d5474b36a36ffdb324251101');
